6bc526487c0806be3b3e62805872489fccd6e6f4
wendycorene <s524779@mail.nwmissouri.edu>
2017-10-25 10:48:42 -0500

    Added the Login and Register features with Backendless.  It took FOREVER, but I FINALLY DID IT!!!!  YAY!!!  Let me know if you have questions.  Feel free to make your own user.  If you don't want to make a user, feel free to use the username: guest and password: guest.

--- app/src/main/java/com/example/s528748/trackerapp/MainActivity.java
+++ app/src/main/java/com/example/s528748/trackerapp/MainActivity.java
@@ -4,13 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String APP_ID = "FC6F63F0-859B-84B5-FF4D-126C761AC700";
+    public static final String API_KEY = "64D916B7-2C32-39D6-FF3E-E60682A88200";
+    public static final String SERVER_URL = "https://api.backendless.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Backendless.initApp(this, APP_ID, API_KEY);
+
     }
 
     public void signUp(View view) {
@@ -21,7 +35,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void login(View view) {
         // go to home after logging in
-        Intent i = new Intent(this, Home.class);
-        startActivity(i);
+        EditText username = (EditText) findViewById(R.id.usernameLogin);
+        EditText password = (EditText) findViewById(R.id.passwordLogin);
+        String user = username.getText().toString();
+        String pwd = password.getText().toString();
+        Backendless.UserService.login(user, pwd, new AsyncCallback<BackendlessUser>()
+        {
+            public void handleResponse( BackendlessUser user )
+            {
+                startActivity(new Intent(MainActivity.this, Home.class));
+            }
+
+            public void handleFault( BackendlessFault fault )
+            {
+                Toast.makeText(getApplicationContext(), "There was a problem with your " +
+                        "login", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/s528748/trackerapp/Signup.java
+++ app/src/main/java/com/example/s528748/trackerapp/Signup.java
@@ -1,13 +1,71 @@
 package com.example.s528748.trackerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 
 public class Signup extends AppCompatActivity {
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private EditText phoneField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button register = (Button) findViewById(R.id.registerBTN);
+        usernameField = (EditText) findViewById(R.id.usernameET);
+        passwordField = (EditText) findViewById(R.id.passwordET);
+        phoneField = (EditText) findViewById(R.id.phoneET);
+
+        register.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = usernameField.getText().toString();
+                String password = passwordField.getText().toString();
+                String phone = phoneField.getText().toString();
+
+                //Register a user with Backendless
+                BackendlessUser backendlessUser = new BackendlessUser();
+                backendlessUser.setPassword(password);
+                backendlessUser.setProperty("name", username);
+                backendlessUser.setProperty("phone", phone);
+
+                //Actually registers user with Backendless
+                Backendless.UserService.register(backendlessUser, new AsyncCallback<BackendlessUser>() {
+                    @Override
+                    public void handleResponse(BackendlessUser response) {
+                        Toast.makeText(getApplicationContext(), "You are registered with username: "
+                                + username, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        Toast.makeText(getApplicationContext(), "There was a problem with your " +
+                                "registration", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        Button back = (Button)findViewById(R.id.backToLogin);
+        back.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Signup.this, MainActivity.class));
+            }
+        });
     }
+
 }
