82d8145c60fa6ad6a0775799b54ca0f0153fe115
Swaroopg79 <s528802@mail.nwmissouri.edu>
2017-11-17 22:54:09 -0600

    After including zoom functionality to maps

--- app/src/main/java/com/example/s528748/trackerapp/MapsActivity.java
+++ app/src/main/java/com/example/s528748/trackerapp/MapsActivity.java
@@ -46,22 +46,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(40.362552, -94.767392);
 
         mMap.addMarker(new MarkerOptions().position(sydney).title("Joseph Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.362552,-94.767392), 12.0f));
 
         LatLng publicUser1 = new LatLng(40.332692, -94.870458);
         mMap.addMarker(new MarkerOptions().position(publicUser1).title("David Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser1));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser1));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.332692,-94.870458), 12.0f));
 
         LatLng publicUser2 = new LatLng(40.350650, -94.886080);
         mMap.addMarker(new MarkerOptions().position(publicUser2).title("Alex Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser2));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser2));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.350650,-94.886080), 12.0f));
 
         LatLng publicUser3 = new LatLng(40.357818, -94.890868);
         mMap.addMarker(new MarkerOptions().position(publicUser3).title("Johnson Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser1));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser1));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.357818,-94.890868), 12.0f));
 
         LatLng publicUser4 = new LatLng(40.348272, -94.871527);
         mMap.addMarker(new MarkerOptions().position(publicUser4).title("Mike Location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser2));
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(publicUser2));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.348272,-94.871527), 12.0f));
     }
 }
